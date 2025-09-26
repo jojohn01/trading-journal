3832af07a84eeadd28aeef59c2df177b5975755b
jojohn01 <jojohnofuya@gmail.com>
2025-09-26 00:03:13 -0600

    Update .gitignore, settings.py, and requirements.txt
    - Modified .gitignore to improve file exclusions
    - Updated config/settings.py with new configuration changes
    - Adjusted requirements.txt for dependency updates

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
-    # "allauth.socialaccount.providers.google",  # ← add later when ready
+    "allauth.socialaccount.providers.google",  # ← add later when ready
 
 
 ]
